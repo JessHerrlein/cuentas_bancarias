d74c812e534f1db7db250f6e4543bb1beb8d4c45
JessHerrlein <33031069+JessHerrlein@users.noreply.github.com>
2017-10-30 20:41:54 -0300

    Add files via upload

---
+++ Operacion.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Cuentas_bancarias
+{
+    public class Operacion
+    {
+        public DateTime Fecha { get; set;}
+        public decimal Monto { get; set;}
+        public string Tipo { get; set;}
+        
+    }
+
+
+}
